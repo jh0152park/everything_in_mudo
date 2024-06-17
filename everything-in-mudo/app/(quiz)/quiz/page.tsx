024a1b42bb8a32931d692240dfc2cbeda9f2690f
jh0152park <jh0152park@naver.com>
2024-06-18 01:41:54 +0900

    fixed refreshed issue

--- everything-in-mudo/app/(quiz)/quiz/page.tsx
+++ everything-in-mudo/app/(quiz)/quiz/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
-import { HINT } from "@/global/answer";
+import { ANSWER, HINT } from "@/global/answer";
 import { DIFFICULTY, LEVELS, QUIZ_COUNT } from "@/global/project-common";
 import { generate_quiz_sequence } from "@/utils/generate-quiz-sequence";
 
@@ -18,23 +18,26 @@ import { generate_quiz_sequence } from "@/utils/generate-quiz-sequence";
 
 export default function Quiz() {
     const [quiz, setQuiz] = useState(1);
-    const [correct, setCorrect] = useState(0);
+    const [sequence, setSequence] = useState<number[]>([]);
     const [isSubmitted, setIsSubmitted] = useState(false);
 
     const quiz_count = QUIZ_COUNT[DIFFICULTY];
-    const sequence = generate_quiz_sequence();
+    const { register, setValue, handleSubmit } = useForm();
+    // const sequence = generate_quiz_sequence();
+    let correcct = 0;
 
-    const { register, reset, handleSubmit } = useForm();
-
-    console.log(DIFFICULTY);
-    console.log(LEVELS[DIFFICULTY]);
-    console.log(sequence);
-    console.log(quiz_count);
+    // console.log(DIFFICULTY);
+    // console.log(LEVELS[DIFFICULTY]);
+    // console.log(sequence);
+    // console.log(quiz_count);
 
     function onSubmit(data: FieldValues) {
-        console.log(data);
         setIsSubmitted(true);
-        reset();
+        setValue("answer", "");
+
+        if (ANSWER[sequence[quiz - 1]] === data.answer) {
+            correcct++;
+        }
     }
 
     function onNextClick() {
@@ -45,6 +48,10 @@ export default function Quiz() {
         setQuiz((prev) => prev + 1);
         setIsSubmitted(false);
     }
+
+    useEffect(() => {
+        setSequence(generate_quiz_sequence());
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center w-full h-screen *:font-bold text-3xl gap-10 p-5">
@@ -63,7 +70,8 @@ export default function Quiz() {
                 }
                 alt={`${sequence[quiz - 1]}.question`}
                 width="600"
-                height="500"
+                height="400"
+                priority={true}
             />
 
             <div className="flex flex-col items-center justify-center w-full h-10 ">
@@ -77,21 +85,25 @@ export default function Quiz() {
                 )}
             </div>
 
-            <form onSubmit={handleSubmit(onSubmit)} className="w-1/2 h-12 ">
-                <input
-                    className="w-full h-full px-3 py-1 bg-[#e7e8da] placeholder:text-[#7f7377] placeholder:text-center border-b-2 border-[#7f7377] focus:outline-none text-2xl placeholder:text-2xl"
-                    placeholder="당신의 무도력은?!"
-                    autoFocus
-                    {...register("answer", { required: true })}
-                />
-            </form>
+            {!isSubmitted && (
+                <form onSubmit={handleSubmit(onSubmit)} className="w-1/2 h-12 ">
+                    <input
+                        className="w-full h-full px-3 py-1 bg-[#e7e8da] placeholder:text-[#7f7377] placeholder:text-center border-b-2 border-[#7f7377] focus:outline-none text-2xl placeholder:text-2xl"
+                        placeholder="당신의 무도력은?!"
+                        autoFocus
+                        {...register("answer", { required: true })}
+                    />
+                </form>
+            )}
 
-            <div
-                onClick={onNextClick}
-                className="w-1/2 py-1.5 text-2xl text-center rounded-md bg-[#7f7377] text-[#e7e8da] transition-all hover:cursor-pointer hover:bg-[#e7e8da] hover:text-[#7f7377] border border-[#7f7377]"
-            >
-                다음 문제
-            </div>
+            {isSubmitted && (
+                <div
+                    onClick={onNextClick}
+                    className="w-1/2 py-1.5 text-2xl text-center rounded-md bg-[#7f7377] text-[#e7e8da] transition-all hover:cursor-pointer hover:bg-[#e7e8da] hover:text-[#7f7377] border border-[#7f7377]"
+                >
+                    다음 문제
+                </div>
+            )}
         </div>
     );
 }
