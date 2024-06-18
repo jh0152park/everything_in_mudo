2e6f2d961660f022e700f5002f17e4d44d744045
jh0152park <jh0152park@naver.com>
2024-06-18 22:30:51 +0900

    added instruction page responsible

--- everything-in-mudo/app/(instruction)/instruction/page.tsx
+++ everything-in-mudo/app/(instruction)/instruction/page.tsx
@@ -3,15 +3,15 @@ import LevelButton from "@/components/project-common/level-button";
 
 export default function Instruction() {
     return (
-        <main className="flex flex-col items-center justify-center w-full h-screen ">
-            <div className="flex items-center justify-center w-1/2 gap-4 h-1/4">
-                <div className="flex flex-col items-end justify-center w-1/2 h-full text-xl font-bold">
+        <main className="flex flex-col items-center justify-center w-full h-screen p-5">
+            <div className="flex items-center justify-center w-full gap-4 h-1/4 text-[13px] sm:text-xl">
+                <div className="flex flex-col items-end justify-center w-1/2 h-full font-bold">
                     <span>? ! . ,와 같은 문장기호는 X</span>
                     <span>하지만 공백은 포함!</span>
                     <span>문제 순서는 랜덤!</span>
                 </div>
                 <div className=" w-[1px] h-full border border-[#7f7377]" />
-                <div className="flex flex-col items-start justify-center w-1/2 h-full text-xl font-bold">
+                <div className="flex flex-col items-start justify-center w-1/2 h-full font-bold">
                     <span>초급: 20문제, 초성 O, 공백 O</span>
                     <span>중급: 30문제, 초성 O, 공백 X</span>
                     <span>고급: 50문제, 초성 X, 공백 X</span>
