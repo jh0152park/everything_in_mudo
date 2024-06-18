29892a1f5045627661cf45fb84d1a5798eae6cb3
jh0152park <jh0152park@naver.com>
2024-06-18 18:14:04 +0900

    fixed z-index

--- everything-in-mudo/app/(instruction)/instruction/page.tsx
+++ everything-in-mudo/app/(instruction)/instruction/page.tsx
@@ -26,10 +26,15 @@ export default function Instruction() {
 
             <Image
                 src="/resources/image/etc/members.jpeg"
-                width="500"
-                height="200"
                 alt="members"
-                className="fixed left-auto right-auto bottom-4"
+                className="fixed left-auto right-auto z-0 bottom-4"
+                width="600"
+                height="250"
+                style={{
+                    width: "auto",
+                    height: "auto",
+                    objectFit: "cover",
+                }}
             />
         </main>
     );
--- everything-in-mudo/components/project-common/level-button.tsx
+++ everything-in-mudo/components/project-common/level-button.tsx
@@ -30,7 +30,7 @@ export default function LevelButton({ title, path }: IButton) {
     return (
         <div
             onClick={onClick}
-            className=" px-3 py-1.5 bg-[#7f7377] rounded-md group hover:cursor-pointer hover:bg-[#e7e8da] *:transition-all border border-[#e7e7da] hover:border-[#7f7377]"
+            className=" z-10 px-3 py-1.5 bg-[#7f7377] rounded-md group hover:cursor-pointer hover:bg-[#e7e8da] *:transition-all border border-[#e7e7da] hover:border-[#7f7377]"
         >
             <span className=" text-[#e7e8da] font-bold group-hover:text-[#7f7377]">
                 {title}
